647459756e01175a33f4d02b34a916ec6537036c
isabellozano37 <isabeloficial37@gmail.com>
2023-09-22 13:33:28 +0200

    Changes

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client'
 import Header from './Components/Header/Header'
 import Carousel from './Components/Carousel/Carousel'
 import Menu from './Components/Menu/Menu'
+import Login from './Components/Login/Login'
 //import Audioplayer from './Components/Audioplayer/Audioplayer.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+/*ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Header />
     <Menu />
     <Carousel />
+  </React.StrictMode>,
+)*/
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Login />
   </React.StrictMode>,
 )
