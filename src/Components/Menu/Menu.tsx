44a2d9e92b3e14a01a2b0aaa708e0d4ce38e4dc9
isabellozano37 <isabeloficial37@gmail.com>
2023-09-22 10:14:08 +0200

    Changes

--- src/Components/Menu/Menu.tsx
+++ src/Components/Menu/Menu.tsx
@@ -1,15 +1,38 @@
 import "./Menu.css";
+import { useState } from "react";
 
 export default function Menu() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <div className="Container">
       <div className="boxSong">
         <button className="loginbutton">
-        <img className="home" src="https://res.cloudinary.com/da7ffijqs/image/upload/v1695323366/images-removebg-preview_1_xuayhf.png" alt="" />
+          <img
+            className="home"
+            src="https://res.cloudinary.com/da7ffijqs/image/upload/v1695323366/images-removebg-preview_1_xuayhf.png"
+            alt=""
+          />
         </button>
-        <button className="registrobutton">
-          <img className="type" src="" alt="" />
+        <button className="registrobutton" onClick={toggleDropdown}>
+          <img
+            className="categorie"
+            src="https://res.cloudinary.com/da7ffijqs/image/upload/v1695367319/musica-removebg-preview_scp9yc.png"
+            alt=""
+          />
         </button>
+        {isDropdownOpen && (
+          <div className="dropdown-content">
+            <a href="#">SoundTracks</a>
+            <a href="#">Peliculas Animadas</a>
+            <a href="#">Clasicas</a>
+            <a href="#">Actuales</a>
+          </div>
+        )}
       </div>
       <div className="boxuser">
         <button className="loginbutton">
@@ -30,3 +53,4 @@ export default function Menu() {
     </div>
   );
 }
+
