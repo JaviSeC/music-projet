fefcb3494e20f219b68bd508e7553affad850d75
Javiser <jascjav@gmail.com>
2023-09-27 13:25:49 +0200

    comit changes

--- src/main.tsx
+++ src/main.tsx
@@ -10,13 +10,7 @@ import Tops from './Components/Tops/Tops';
 // import SignUp from './Components/SingUp/SingUp'
 import './index.css'
 import Footer from './Components/Footer/Footer'
-
-
-
-
-
-
-
+import Admin from './Components/Adminview/Admin';
 
 // ReactDOM.createRoot(document.getElementById('root')!).render(
 //   <React.StrictMode>
@@ -28,19 +22,27 @@ import Footer from './Components/Footer/Footer'
 //   </React.StrictMode>
 // )
 
+//____________________________________________________________________________________________________
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//     <React.StrictMode>
+//       <NextUIProvider>
+//       <Header /> 
+//       <Menu />
+//       <Body />
+//      <Tops/>
+//      <Carousel />
+   
+//       <Footer />
+//       </NextUIProvider>
+//     </React.StrictMode>,
+//   )
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <NextUIProvider>
-      <Header /> 
-      <Menu />
-      <Body />
-     <Tops/>
-     <Carousel />
-   
+      <Admin />
       <Footer />
       </NextUIProvider>
     </React.StrictMode>,
   )
-
-
