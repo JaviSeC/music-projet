56aead22e199eba7b181566c6bbe0a36d4255f55
miguelcidoncha <cidoncha1996@gmail.com>
2023-09-28 12:03:23 +0200

    add component in PageInitial

--- src/Views/PageInitial/PageInitial.tsx
+++ src/Views/PageInitial/PageInitial.tsx
@@ -2,6 +2,7 @@ import Body from "../../Components/Body/Body";
 import Carousel from "../../Components/Carousel/Carousel";
 import Header from "../../Components/Header/Header";
 import Menu from "../../Components/Menu/Menu";
+import Tops from "../../Components/Tops/Tops";
 
 export default function PageInitial() {
   return (
@@ -9,6 +10,7 @@ export default function PageInitial() {
       <Header />
       <Menu />
       <Body />
+      <Tops />
       <Carousel />
     </>
   );
