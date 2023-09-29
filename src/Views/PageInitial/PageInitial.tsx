33309d0887c5d6d48a773c1c2152a5a1833a0998
isabellozano37 <isabeloficial37@gmail.com>
2023-09-29 10:13:08 +0200

    Changes

--- src/Views/PageInitial/PageInitial.tsx
+++ src/Views/PageInitial/PageInitial.tsx
@@ -1,8 +1,6 @@
 import Body from "../../Components/Body/Body";
 import Carousel from "../../Components/Carousel/Carousel";
-import BodyClassic from "../../Components/ClassicBody/BodyClassic";
 import Header from "../../Components/Header/Header";
-import Headerunico from "../../Components/Headerunico/Headerunico";
 import Menu from "../../Components/Menu/Menu";
 import Tops from "../../Components/Tops/Tops";
 import "./PageInitial.css";
