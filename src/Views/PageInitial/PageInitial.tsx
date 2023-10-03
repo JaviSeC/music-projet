bbdf867c9153ac20a70948125a005a1de5c6c8fe
isabellozano37 <isabeloficial37@gmail.com>
2023-10-03 09:33:55 +0200

    Chnages

--- src/Views/PageInitial/PageInitial.tsx
+++ src/Views/PageInitial/PageInitial.tsx
@@ -1,35 +1,25 @@
-<<<<<<< HEAD
 import Body from "../../Components/Body/Body";
 import Carousel from "../../Components/Carousel/Carousel";
-=======
->>>>>>> origin/Javier
 import Header from "../../Components/Header/Header";
 import Menu from "../../Components/Menu/Menu";
-import Body from "../../Components/Body/Body";
 import Tops from "../../Components/Tops/Tops";
-import Carousel from "../../Components/Carousel/Carousel";
 import "./PageInitial.css";
 
 export default function PageInitial() {
   return (
     <>
-<<<<<<< HEAD
       <Header />
       <div className="home">
       <Menu />
       <Body />
       <Tops />
       </div>
-=======
-       
         <Header />
         <div className="home">
           <Menu />
           <Body />
           <Tops />
         </div>
-      
->>>>>>> origin/Javier
       <Carousel />
     </>
   );
