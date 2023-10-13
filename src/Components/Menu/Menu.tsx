072580e8f18a4a3921654881b36590e3238e6793
Rodrigo <ramunozg@gmail.com>
2023-10-13 10:40:08 +0200

    Desplegable New

--- src/Components/Menu/Menu.tsx
+++ src/Components/Menu/Menu.tsx
@@ -2,11 +2,81 @@ import "./Menu.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
+// export default function Menu() {
+//   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+//   const toggleDropdown = () => {
+//     setIsDropdownOpen(!isDropdownOpen);
+//   };
+
+  
+//   return (
+//     <div className="Container">
+//       <div className="boxSong">
+//         <Link to="/">
+//           <button className="loginbutton">
+//             <img
+//               className="home"
+//               src="https://res.cloudinary.com/da7ffijqs/image/upload/v1695323366/images-removebg-preview_1_xuayhf.png"
+//               alt=""
+//             />
+//           </button>
+//         </Link>
+//         <button className="registrobutton" onClick={toggleDropdown}>
+//           <img
+//             className="categorie"
+//             src="https://res.cloudinary.com/da7ffijqs/image/upload/v1695367319/musica-removebg-preview_scp9yc.png"
+//             alt=""
+//           />
+//         </button>
+//         {isDropdownOpen && (
+//           <div className="dropdown-content">
+//             <a href="/SoundTracksView">Ganadores Oscar🏆</a>
+//             <a href="/PeliculasAnimadasView">Animadas🧸</a>
+//             <a href="/ClassicView">Clasicas📀</a>
+//             <a href="#">Terror👻</a>
+//           </div>
+//         )}
+//       </div>
+//       <div className="boxuser">
+//         <Link to="/PageLogin">
+//           <button className="loginbutton">
+//             <img
+//               className="login"
+//               src="https://res.cloudinary.com/da7ffijqs/image/upload/v1695319832/_8aeb1ec2-ba3a-4b6d-a850-a142ccb46b8a-removebg-preview_cfwx19.png"
+//               alt=""
+//             />
+//           </button>
+//         </Link>
+//         <Link to="/PageSingUp">
+//           <button className="registrobutton">
+//             <img
+//               className="registro"
+//               src="https://res.cloudinary.com/da7ffijqs/image/upload/v1695321417/grid_landscape-removebg-preview_smbjbe.png"
+//               alt=""
+//             />
+//           </button>
+//         </Link>
+
+//         <Link to="/PageAdmin">
+//           <button>
+//            <strong> Admin </strong>
+//           </button>
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// }
+
 export default function Menu() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
+  const handleMouseEnter = () => {
+    setIsDropdownOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsDropdownOpen(false);
   };
 
   return (
@@ -21,21 +91,21 @@ export default function Menu() {
             />
           </button>
         </Link>
-        <button className="registrobutton" onClick={toggleDropdown}>
+        <button className="registrobutton" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
           <img
             className="categorie"
             src="https://res.cloudinary.com/da7ffijqs/image/upload/v1695367319/musica-removebg-preview_scp9yc.png"
             alt=""
           />
+          {isDropdownOpen && (
+            <div className="dropdown-content">
+              <a href="/SoundTracksView">Ganadores Oscar🏆</a>
+              <a href="/PeliculasAnimadasView">Animadas🧸</a>
+              <a href="/ClassicView">Clasicas📀</a>
+              <a href="#">Terror👻</a>
+            </div>
+          )}
         </button>
-        {isDropdownOpen && (
-          <div className="dropdown-content">
-            <a href="/SoundTracksView">Ganadores Oscar🏆</a>
-            <a href="/PeliculasAnimadasView">Animadas🧸</a>
-            <a href="/ClassicView">Clasicas📀</a>
-            <a href="#">Terror👻</a>
-          </div>
-        )}
       </div>
       <div className="boxuser">
         <Link to="/PageLogin">
@@ -59,7 +129,7 @@ export default function Menu() {
 
         <Link to="/PageAdmin">
           <button>
-           <strong> Admin </strong>
+            <strong> Admin </strong>
           </button>
         </Link>
       </div>
