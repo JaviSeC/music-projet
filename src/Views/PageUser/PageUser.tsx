bb3b85b083c6a80bbee9b9247d543ea0b60eb90e
isabellozano37 <isabeloficial37@gmail.com>
2023-10-19 23:34:50 +0200

    Changes

--- src/Views/PageUser/PageUser.tsx
+++ src/Views/PageUser/PageUser.tsx
@@ -1,10 +1,12 @@
 import Headerunico from "../../Components/Headerunico/Headerunico";
+import Users from "../../Components/Users/Users";
 
 
 export default function PageUser() {
   return (
     <>
       <Headerunico />
+      <Users/>
     </>
   );
 }
