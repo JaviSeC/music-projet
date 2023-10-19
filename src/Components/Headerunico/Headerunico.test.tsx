19180ba3d1419bf638abdbd3e2dcd550090c54d7
miguelcidoncha <cidoncha1996@gmail.com>
2023-10-19 09:20:40 +0200

    Headerunico.test

--- src/Components/Headerunico/Headerunico.test.tsx
+++ src/Components/Headerunico/Headerunico.test.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Headerunico from './Headerunico';
 
 describe('Headerunico component', () => {
   it('renders logo', () => {
-    render(<Headerunico />);
-    const logoElement = screen.getByAltText('logo');
-    expect(logoElement).toBeTruthy();
+    render(
+      <Router>
+        <Headerunico />
+      </Router>
+    );
+    const logoElement = screen.getByAltText('logo') as HTMLImageElement;
+    expect(logoElement).toBeDefined();
   });
 
   it('renders title', () => {
-    render(<Headerunico />);
-    const titleElement = screen.getByText('Sound Tracks Magic');
-    expect(titleElement).toBeTruthy();
+    render(
+      <Router>
+        <Headerunico />
+      </Router>
+    );
+    const titleElement = screen.getByText(/Sound Tracks Magic/i);
+    expect(titleElement).toBeDefined();
+  });
+
+  it('renders link to home', () => {
+    render(
+      <Router>
+        <Headerunico />
+      </Router>
+    );
+    const linkElement = screen.getByRole('link', { href: '/' });
+    expect(linkElement).toBeDefined();
   });
 });
