38cfe7b1e517eb6e2b150e2853d4e079f14e954e
miguelcidoncha <cidoncha1996@gmail.com>
2023-10-19 12:11:34 +0200

    Signup.test

--- src/Components/SingUp/SingUp.test.tsx
+++ src/Components/SingUp/SingUp.test.tsx
@@ -14,33 +14,5 @@ describe('SignUp component', () => {
     expect(linkElements.length).toBeGreaterThan(0);
   });
 
-  test('submitting form with valid data', () => {
-    render(
-      <BrowserRouter>
-        <SignUp />
-      </BrowserRouter>
-    );
-    const submitButton = screen.getByRole('button', { name: /Sign Up/i });
-
-    // Fill in the form
-    fireEvent.change(screen.getByLabelText(/First Name/i), {
-      target: { value: 'John' },
-    });
-    fireEvent.change(screen.getByLabelText(/Last Name/i), {
-      target: { value: 'Doe' },
-    });
-    fireEvent.change(screen.getByLabelText(/UserName/i), {
-      target: { value: 'johndoe' },
-    });
-    fireEvent.change(screen.getByLabelText(/Email Address/i), {
-      target: { value: 'johndoe@example.com' },
-    });
-    fireEvent.change(screen.getByLabelText(/Password/i), {
-      target: { value: 'password123' },
-    });
-
-    fireEvent.click(submitButton);
-
-    // Add your assertions here based on the expected behavior after form submission
-  });
+ 
 });
